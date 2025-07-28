c747cee28e1ffd168c90c341a4627f549807da81
faizamairaj <faizamairaj0@gmail.com>
2025-07-28 12:02:29 +0500

    student-management-hackathon

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -9,7 +9,7 @@ export default function AdminLogin() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/admin/login', form);
+      const res = await axios.post('https://student-management-hackathon.vercel.app/', form);
       localStorage.setItem('token', res.data.token);
       navigate('/admin/dashboard');
     } catch (error) {
